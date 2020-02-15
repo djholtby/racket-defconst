466b3f713ac3c990a83aefdf8664247465d84cb6
Dan Holtby <djholtby@gmail.com>
2020-02-15 14:19:36 -0500

    update info

--- info.rkt
+++ info.rkt
@@ -3,6 +3,6 @@
 (define deps '("base"))
 (define build-deps '("scribble-lib" "racket-doc" "rackunit-lib"))
 (define scribblings '(("scribblings/defconst.scrbl" ())))
-(define pkg-desc "Description Here")
-(define version "0.0")
+(define pkg-desc "A module for compactly defining multiple constants")
+(define version "1.0")
 (define pkg-authors '(djholtby))
